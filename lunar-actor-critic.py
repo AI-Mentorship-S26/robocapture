0f89ce1845b023ec396356b9284ebd06c3089fbb
JonasSBanks <130476513+JonasSBanks@users.noreply.github.com>
2026-03-25 21:44:38 -0500

    Update lunar-actor-critic.py

--- lunar-actor-critic.py
+++ lunar-actor-critic.py
@@ -5,33 +5,50 @@ import torch.nn.functional as F
 
 import gymnasium as gym
 
-class ActorCritic(nn.Module):
+class Actor(nn.Module):
     def __init__(self, n_inputs, n_actions):
-        super(ActorCritic, self).__init__()
+        super(Actor, self).__init__()
         
-        self.shared = nn.Sequential(
-            nn.Linear(n_inputs, 128),
+        self.actor = nn.Sequential(
+            nn.Linear(n_inputs, 64),
             nn.ReLU(),
-            nn.Linear(128, 128),
+            nn.Linear(64, 64),
             nn.ReLU(),
-            nn.Linear(128, 128),
+            nn.Linear(64, 64),
             nn.ReLU(),
+            nn.Linear(64, n_actions),
+            nn.Softmax(dim=-1)
         )
-        
-        self.actor = nn.Linear(128, n_actions)
-        self.critic = nn.Linear(128, 1)
 
     def forward(self, x):
-        features = self.shared(x)
-        action_probs = F.softmax(self.actor(features), dim=-1)
-        state_value = self.critic(features)
-        return action_probs, state_value
+        return self.actor(x)
+
+class Critic(nn.Module):
+    def __init__(self, n_inputs):
+        super(Critic, self).__init__()
+
+        self.critic = nn.Sequential(
+            nn.Linear(n_inputs, 64),
+            nn.ReLU(),
+            nn.Linear(64, 64),
+            nn.ReLU(),
+            nn.Linear(64, 64),
+            nn.ReLU(),
+            nn.Linear(64, 1)
+        )
+
+    def forward(self, x):
+        return self.critic(x)
+        
     
 env_train = gym.make("LunarLander-v3")
 env_viz = gym.make("LunarLander-v3", render_mode="human")
 
-ac_model = ActorCritic(8, 4)
-optimizer = optim.Adam(ac_model.parameters(), lr=1e-5)
+actor_model = Actor(8, 4)
+critic_model = Critic(8)
+actor_optimizer = optim.Adam(actor_model.parameters(), lr=1e-3)
+critic_optimizer = optim.Adam(critic_model.parameters(), lr=5e-3)
+
     
 state, info = env_train.reset()
 state, info = env_viz.reset()
@@ -46,30 +63,36 @@ for episode in range(10000):
 
     while not done:
         state_tensor = torch.from_numpy(state).float()
-        probs, state_value = ac_model(state_tensor)
+        probs = actor_model(state_tensor)
 
         m = torch.distributions.Categorical(probs)
         action = m.sample()
+
+        value = critic_model(state_tensor)
 
         next_state, reward, terminated, truncated, _ = env.step(action.item())
         done = terminated or truncated
 
         next_state_tensor = torch.from_numpy(next_state).float()
-        _, next_state_value = ac_model(next_state_tensor)
+        next_state_value = critic_model(next_state_tensor)
 
-        if done:
-            next_state_value = torch.tensor([0.0])
+        td_target = reward + (0.99 * next_state_value * (1 - int(done)))
+        advantage = td_target - value
 
-        target = reward + 0.99 * next_state_value
-        advantage = target - state_value
 
         actor_loss = -m.log_prob(action) * advantage.detach()
-        critic_loss = F.mse_loss(state_value, target.detach())
-        total_loss = actor_loss + critic_loss
+        critic_loss = F.mse_loss(value, td_target.detach())
 
-        optimizer.zero_grad()
-        total_loss.backward()
-        optimizer.step()
+
+        actor_optimizer.zero_grad()
+        critic_optimizer.zero_grad()
+
+
+        actor_loss.backward()
+        actor_optimizer.step()
+
+        critic_loss.backward()
+        critic_optimizer.step()
         
         episode_reward += reward
         state = next_state
