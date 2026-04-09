c8413d1a4443ab2fb0ce9675a919dcfb7850c67c
JonasSBanks <130476513+JonasSBanks@users.noreply.github.com>
2026-04-08 20:44:38 -0500

    whatever

--- backend/obj/Debug/net10.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net10.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+462c5f8f25084027a739f57bc6a41b3fb279b960")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f81e6f13940dc39d3f3d05d9b7c8488cd1fece23")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
