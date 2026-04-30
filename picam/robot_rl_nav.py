9f81a29e38ec7ea6f6a141e51d7d00d6b0db7afa
Sadeem Ziyan <sadeemziyan@gmail.com>
2026-04-29 19:40:19 -0500

    changed model

--- picam/robot_rl_nav.py
+++ picam/robot_rl_nav.py
@@ -90,7 +90,7 @@ NAV_SCORE_MAP = {
     "tiny_sac":          nav_score_tiny_sac,
 }
 
-current_model = "sarsa"
+current_model = "deep_contextual_bandit"
 pipeline      = ImagePreprocessingPipeline()
 
 def set_current_model(model_name: str):
